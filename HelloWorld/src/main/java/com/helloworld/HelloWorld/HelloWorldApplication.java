098716f243e08a73e19f96c2df0e649917dabba5
ganesh <ganeshpasupuleti800@gmail.com>
2025-04-06 05:11:19 -0400

    my second commit

---
+++ HelloWorld/src/main/java/com/helloworld/HelloWorld/HelloWorldApplication.java
@@ -0,0 +1,16 @@
+package com.helloworld.HelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+		
+		System.out.println("Hello Welcome to Spring Boot");
+		
+	}
+
+}
